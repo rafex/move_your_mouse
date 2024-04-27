196e5bbfd51c37a97ff13f25aaeca51d6ed19182
rafex <rafex@rafex.dev>
2024-04-27 00:46:44 -0600

    ya compila y jala

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 PWD=$(pwd)
 export PATH_MY_SCRIPT=$PWD/scripts-utils-containers
 
-source $PWD/scripts-utils-containers/build.sh -V -n MoveYourMouse -d $PWD/docker/Dockerfile.compile
+DOCKERFILE_COMPILE=$PWD/docker/Dockerfile.compile
+
+source $PWD/scripts-utils-containers/build.sh -V -n moveyourmouse-compile -d $DOCKERFILE_COMPILE

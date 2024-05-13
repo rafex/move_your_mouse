51d9a64909325eae57326f6c9baae348db19ef6b
rafex <rafex@rafex.dev>
2024-05-12 21:22:09 -0600

    coverage

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 PWD=$(pwd)
 export PATH_MY_SCRIPT=$PWD/scripts-utils-containers
 
-DOCKERFILE_COMPILE=$PWD/docker/Dockerfile.compile
+DOCKERFILE_COMPILE=$PWD/docker/Dockerfile.graalvm
 
 source $PWD/scripts-utils-containers/build.sh -V -n moveyourmouse-compile -d $DOCKERFILE_COMPILE
